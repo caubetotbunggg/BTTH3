64438bd4aba33f96ab6ef5d2d68a22294f94759d
caubetotbunggg <166147553+caubetotbunggg@users.noreply.github.com>
2025-05-23 09:43:20 +0700

    Initial commit

---
+++ tesing.py
@@ -0,0 +1,5 @@
+import numpy,uvicorn,fastapi,requests,black,pylint,isort
+
+hehe= 3
+for xinchao in range(hehe) :
+    print( "toi yeu kiaisoft" )
---
+++ testing.py
@@ -0,0 +1,11 @@
+import black
+import fastapi
+import isort
+import numpy
+import pylint
+import requests
+import uvicorn
+
+num = 3
+for i in range(num):
+    print("toi yeu kiaisoft")
