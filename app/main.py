43081a6f80be266dd009cde4d9bb70fcf5719daa
caubetotbunggg <166147553+caubetotbunggg@users.noreply.github.com>
2025-09-22 01:19:46 +0700

    chore(monitoring): config instrumentator

--- app/main.py
+++ app/main.py
@@ -5,20 +5,13 @@ from app.controllers import health_controller, rag_controller, retrieve_controll
 
 app = FastAPI()
 
-instrumentator = Instrumentator(
-    should_group_status_codes=True,
-    should_ignore_untemplated=False,
-    should_respect_env_var=True,
-    should_instrument_requests_inprogress=True,
-    excluded_handlers=["/metrics"],
-)
-
-instrumentator.instrument(app).expose(app)
 
 app.include_router(retrieve_controller.router)
 app.include_router(rag_controller.router)
 app.include_router(tools_controller.router)
 app.include_router(health_controller.router)
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
